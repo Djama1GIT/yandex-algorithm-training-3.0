cdc5233272c6fad41a158f8ee36ff3971721c319
Dj.ama1 <dj.ama1@mail.ru>
2023-04-19 12:36:27 +0300

    1

---
+++ warm-up/1.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+with open("input.txt", 'r') as file:
+    c = Counter()
+    for i in file:
+        string = i.split()
+        for word in string:
+            for letter in word:
+                c[letter] += 1
+    letters = sorted(c.keys())
+    for i in range(max(c.values()), -1, -1):
+        for let in letters:
+            print("#" if c[let] >= i else " ", end="")
+        print()
+    print("".join(letters))
+
